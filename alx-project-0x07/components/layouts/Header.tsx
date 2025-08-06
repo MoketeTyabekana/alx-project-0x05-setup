8739274524f79a253070e4fab188b508de1cad54
Mokete <ematyabekana@gmail.com>
2025-08-06 23:42:55 +0200

    Updated the content of the home pag etp include a forma with header, descriotion ,inout filed and button.

--- alx-project-0x07/components/layouts/Header.tsx
+++ alx-project-0x07/components/layouts/Header.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-gray-800 text-white py-4 ">
+    <header className="bg-gray-800 text-white p-8 shadow-md   ">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
           <h1 className="text-2xl font-bold">ImageGen</h1>
         </div>
-        <nav className="space-x-4 px-10">
+        <nav className="space-x-4 ">
           <Link href="/" className="hover:text-gray-400">Home</Link>
           <Link href="/gallery" className="hover:text-gray-400">Gallery</Link>
           <Link href="/about" className="hover:text-gray-400">About</Link>
